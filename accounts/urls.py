085144b9fd1c75dd37d65e15490b1ef97f62ec71
Bryan Wang <bryankwang06@gmail.com>
2025-09-26 21:39:44 -0400

    start

---
+++ accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('login/', views.login, name='accounts.login'),
+    path('logout/', views.logout, name='accounts.logout'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+# Create your views here.
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('home.index')
+
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html',
+            {'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is None:
+            template_data['error'] ='The username or password is incorrect.'
+            return render(request, 'accounts/login.html',
+                {'template_data': template_data})
+        else:
+            auth_login(request, user)
+            return redirect('home.index')
